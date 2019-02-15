47eb161e51cdc1df1f0f0c21603462cf1c93ac79
Anand Kulkarni <anand.kulkarni.sg@gmail.com>
2019-02-15 22:07:39 +0800

    Anand Kulkarni - 15/02/2019 22:07:38:646 Asia/Singapore(+08,+0800) - Added some fixes to improve replacements to hardcoded values in the Examples..

--- Example1.cpp
+++ Example1.cpp
@@ -8,25 +8,25 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	size_t queueSize=10;
-	ArrayBlockingQueue<int> testQueue(queueSize);
-	vector<int> vec = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
-	bool insertStatus=false;
-	for(unsigned int i=0; i<vec.size(); ++i)
-	{
-		try
-		{
-			insertStatus=testQueue.add(vec[i]);
-			cout << "inserted the item -> " << vec[i] << ", The insertion status = " << insertStatus << endl;
-			cout << testQueue.toString() << endl;
-		}
-		catch(const std::exception& e)
-		{
-			cout << e.what() << endl;
-		}
-	}	
-	cout << "attempting to reset the array" << endl;
-	testQueue.clear();
-	cout << "After the clear the status is : " << endl << testQueue.toString() << endl;	
-	return(0);
+  constexpr size_t queueSize=10;
+  ArrayBlockingQueue<int> testQueue(queueSize);
+  vector<int> vec = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+  bool insertStatus=false;
+  for(unsigned int i=0; i<vec.size(); ++i)
+  {
+    try
+    {
+      insertStatus=testQueue.add(vec[i]);
+      cout << "inserted the item -> " << vec[i] << ", The insertion status = " << insertStatus << endl;
+      cout << testQueue.toString() << endl;
+    }
+    catch(const std::exception& e)
+    {
+      cout << e.what() << endl;
+    }
+  }	
+  cout << "attempting to reset the array" << endl;
+  testQueue.clear();
+  cout << "After the clear the status is : " << endl << testQueue.toString() << endl;	
+  return(0);
 }
--- Example9.cpp
+++ Example9.cpp
@@ -14,45 +14,47 @@ ArrayBlockingQueue<int> blockingQueue(capacity);
 
 void populateQueueRandom(const int& countLimit)
 {
-	int i=0;
-	while(i<countLimit)
-	{
-		blockingQueue.put(rand()%100);		
-		cout << "finished adding an item" << endl;
-		this_thread::sleep_for(chrono::milliseconds(2));
-		++i;
-	}
+  int i=0;
+  while(i<countLimit)
+  {
+    blockingQueue.put(rand()%100);		
+    cout << "finished adding an item" << endl;
+    this_thread::sleep_for(chrono::milliseconds(2));
+    ++i;
+  }
 }
 
 void takeRandomFromQueue(const int& countLimit)
 {
-	int i=0;
-	while(i<countLimit)
-	{
-		blockingQueue.take();
-		cout << "finished taking an item" << endl;
-		this_thread::sleep_for(chrono::milliseconds(2));
-		++i;
-	}
+  int i=0;
+  while(i<countLimit)
+  {
+    blockingQueue.take();
+    cout << "finished taking an item" << endl;
+    this_thread::sleep_for(chrono::milliseconds(2));
+    ++i;
+  }
 }
 
 int main(int argc, char* argv[])
 {
-	vector<thread> putThreadPool , takeThreadPool;
-	putThreadPool.reserve(10);
-	takeThreadPool.reserve(10);
+  vector<thread> putThreadPool , takeThreadPool;
+  constexpr size_t threadPoolSize=10;
+  const int randomLimit=10000;
+  putThreadPool.reserve(threadPoolSize);
+  takeThreadPool.reserve(threadPoolSize);
 
-	for(unsigned int i=0; i<10; ++i)
-		putThreadPool.emplace_back(thread(&populateQueueRandom, 10000));
+  for(unsigned int i=0; i<threadPoolSize; ++i)
+    putThreadPool.emplace_back(thread(&populateQueueRandom, randomLimit));
 
-	for(unsigned int i=0; i<10; ++i)
-		takeThreadPool.emplace_back(thread(&takeRandomFromQueue, 10000));
+  for(unsigned int i=0; i<threadPoolSize; ++i)
+    takeThreadPool.emplace_back(thread(&takeRandomFromQueue, randomLimit));
 
-	for_each(putThreadPool.begin(), putThreadPool.end(), [&](thread& threadItem) { threadItem.join(); });	
-	for_each(takeThreadPool.begin(), takeThreadPool.end(), [&](thread& threadItem) { threadItem.join(); });	
+  for_each(putThreadPool.begin(), putThreadPool.end(), [&](thread& threadItem) { threadItem.join(); });	
+  for_each(takeThreadPool.begin(), takeThreadPool.end(), [&](thread& threadItem) { threadItem.join(); });	
 
-	cout << "The finishing queue status is : " << blockingQueue.toString() << endl;
-	return(0);
+  cout << "The finishing queue status is : " << blockingQueue.toString() << endl;
+  return(0);
 
 }
 
